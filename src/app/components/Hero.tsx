5448d8a6336910826a92fa283067629612462119
Robert Harris <rlharris850@gmail.com>
2025-09-03 09:10:38 -0500

    Add skill badges and a few other adjustments

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,45 +1,52 @@
-import React from 'react'
-import { motion } from 'motion/react'
+import React from 'react';
+import { motion } from 'motion/react';
+
+const skills = ['Progrmaming', 'Design', 'AI', 'Sound Design'];
 
 const Hero = () => {
-  return (
-    <div className='flex flex-col flex-1 p-4'>
-        <motion.h1 
-            className='text-8xl'
+   return (
+      <div className="flex flex-col flex-1 p-4">
+         <motion.h1
+            className="text-8xl"
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
-        >
+         >
             Robert Harris
-        </motion.h1>
-        
-        <motion.p 
-            className='text-2xl'
+         </motion.h1>
+
+         <motion.p
+            className="text-2xl"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.5, duration: 0.8 }}
-        >
+         >
             Self taught Software Engineer
-        </motion.p>
-        
-        <motion.p 
-            className='italic text-gray-200'
+         </motion.p>
+
+         <motion.p
+            className="italic text-gray-200"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1, duration: 0.8 }}
-        >
+         >
             There is always room to improve
-        </motion.p>
-        
-        <motion.div
-            className="mt-4"
-            animate={{ rotate: 360 }}
-            transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
-        >
-            ⚡
-        </motion.div>
-    </div>
-  )
-}
+         </motion.p>
+         <motion.div className="flex gap-4">
+            {skills.map((skill, index) => (
+               <motion.div
+                  key={index}
+                  className="bg-gray-400 p-1 rounded mt-1"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 1, duration: 5 }}
+               >
+                  {skill}
+               </motion.div>
+            ))}
+         </motion.div>
+      </div>
+   );
+};
 
-export default Hero
+export default Hero;
