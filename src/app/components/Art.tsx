8a613f7a8971b5c39ea01bc202a86b1152c745bd
Robert <rlharris850@gmail.com>
2025-09-04 15:24:24 -0500

    Integrate image into Art component, replacing placeholder text with a panoramic view of a futuristic astronaut for enhanced visual content.

--- src/app/components/Art.tsx
+++ src/app/components/Art.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Art = () => {
    return (
       <div className="max-w-4xl w-full text-center font-mono">
          <h1 className="text-6xl mb-8">Art</h1>
-         <p className="text-xl text-gray-200">Coming soon...</p>
+         <div>
+            <Image
+               src="/images/rlharris850_Wide_panoramic_view_of_a_futuristic_astronaut_in__dffe694b-c7d7-426e-88f9-a6e5b8236195_1.png"
+               alt="Futuristic astronaut panoramic view"
+               width={1920}
+               height={1080}
+               className="w-full h-auto object-cover rounded-4xl"
+            ></Image>
+         </div>
       </div>
    );
 };
